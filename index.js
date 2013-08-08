9105e327ed4a6925132cc13e1cd11c00a2eccaf7
MaximTovstashev <max.kitsch@gmail.com>
2013-08-08 10:02:12 +0300

    Actual module structure.

--- index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Admin
- * Date: 02.08.13
- * Time: 12:01
- * To change this template use File | Settings | File Templates.
- */
---
+++ readme.md
@@ -0,0 +1,5 @@
+# bREST
+
+## About
+
+bREST is a simple lib that implements RESTful API over express.js
